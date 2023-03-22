5f7719cbd51748332b7c846d398bf802ca96491d
Tapabrata-Dutta <tapabratadutta777@gmail.com>
2023-03-23 00:48:14 +0530

    Database update

--- database/career.py
+++ database/career.py
@@ -12,23 +12,29 @@ general = Stream("General")
 # types of degree
 computerScience = Degree("B.Tech in Computer Science", "4 Years", Difficulty.Medium)
 informationTechnology = Degree("B.Tech in Information Technology", "4 Years", Difficulty.Medium)
+civilEngineering = Degree("B.Tech in Civil Engineering.", "4 Years", Difficulty.Medium)
 
 # types of career 
-softwareEngineering = Career("Software Engineering", Education.HigherSecondary)
+softwareEngineer = Career("Software Engineer", Education.HigherSecondary)
+civilEngineer = Career("Civil Engineer", Education.HigherSecondary)
 
 # types of questions
 
 
+
 # database degree
-computerScience.addRoadMap(["12th (Science)", "State Level Exam or JEE"])
-informationTechnology.addRoadMap(["12th (Science)", "State Level Exam or JEE"])
+computerScience.addRoadMap(["12th PCM", "State level Exam or National level Exam"])
+informationTechnology.addRoadMap(["12th PCM", "State level Exam or National level Exam"])
+civilEngineering.addRoadMap(["12th PCM", "State level or National level Exam"])
 
 #database career
-softwareEngineering.addDegree(computerScience)
-softwareEngineering.addDegree(informationTechnology)
+softwareEngineer.addDegree(computerScience)
+softwareEngineer.addDegree(informationTechnology)
+civilEngineer.addDegree(civilEngineering)
 
 # database stream
-science.addCareer(softwareEngineering)
+science.addCareer(softwareEngineer)
+science.addCareer(civilEngineer)
 
 # test
 for career in science.getData("careers"):
@@ -39,7 +45,7 @@ for career in science.getData("careers"):
 
     for degree in career.getData("degrees"):
         print("...")
-        print("Name =", degree.getData("name"))
+        print(degree.getData("name"))
         print("Duration =", degree.getData("duration"))
         print("Difficulty =", degree.getData("difficulty").name)
         print("Road Map =", degree.getData("roadMap"))
--- src/mentorCB.py
+++ src/mentorCB.py
@@ -1,31 +1,54 @@
 from enum import Enum
 
-class Career: pass
-class Stream: pass
-class Question: pass
-class Degree: pass
 
-class Education: pass
-class Difficulty: pass
+class Career:
+    pass
+
+
+class Stream:
+    pass
+
+
+class Question:
+    pass
+
+
+class Degree:
+    pass
+
+
+class Education:
+    pass
+
+
+class Difficulty:
+    pass
+
 
 class Stream:
     __name = str
     __likeable = float
     __careers = []
 
-    def __init__(self, name:str) -> None:
+    def __init__(self, name: str) -> None:
         self.__name = name
         self.__likeable = 0.0
-    
-    def addCareer(self, career:Career) -> None:
+
+    def addCareer(self, career: Career) -> None:
         self.__careers.append(career)
         career.changeStream(self)
-    
-    def getData(self, data:str) -> any:
-        if(data == "name"): return self.__name
-        elif(data == "likeable"): return self.__likeable
-        elif(data == "careers"): return self.__careers
-        else: raise Exception("data name = {0} not found in {1} Stream Class".format(data, self.__name))
+
+    def getData(self, data: str) -> any:
+        if (data == "name"):
+            return self.__name
+        elif (data == "likeable"):
+            return self.__likeable
+        elif (data == "careers"):
+            return self.__careers
+        else:
+            raise Exception(
+                "data name = {0} not found in {1} Stream Class".format(data, self.__name))
+
 
 class Career:
     __name = str
@@ -33,45 +56,62 @@ class Career:
     __degrees = []
     __likable = float
     # __demand
-    # __annualSalary 
+    # __annualSalary
     __minimumEducation = Education
 
-    def __init__(self, name:str, minEdu:Education) -> None:
+    def __init__(self, name: str, minEdu: Education) -> None:
         self.__name = name
         self.__likable = 0.0
         self.__minimumEducation = minEdu
 
-    def addDegree(self, degree:Degree):
+    def addDegree(self, degree: Degree):
         self.__degrees.append(degree)
-    
-    def changeStream(self, stream:Stream):
-        if(self.__stream != None): raise Exception("{0} already in {1} so it can't enter {2}".format(self.__name, 
-        self.__stream.getData("name"), stream.getData("name")))
-        else: self.__stream = stream
-    
-    def getData(self, data:str) -> any:
-        if(data == "name"): return self.__name
-        elif(data == "stream"): return self.__stream
-        elif(data == "degrees"): return self.__degrees
-        elif(data == "likable"): return self.__likable
-        elif(data == "minimumEducation"): return self.__minimumEducation
-        else: raise Exception("data name = {0} not found in {1} Career Class".format(data, self.__name))
-    
+
+    def changeStream(self, stream: Stream):
+        if (self.__stream != None):
+            raise Exception("{0} already in {1} so it can't enter {2}".format(self.__name,
+                                                                              self.__stream.getData("name"), stream.getData("name")))
+        else:
+            self.__stream = stream
+
+    def getData(self, data: str) -> any:
+        if (data == "name"):
+            return self.__name
+        elif (data == "stream"):
+            return self.__stream
+        elif (data == "degrees"):
+            return self.__degrees
+        elif (data == "likable"):
+            return self.__likable
+        elif (data == "minimumEducation"):
+            return self.__minimumEducation
+        else:
+            raise Exception(
+                "data name = {0} not found in {1} Career Class".format(data, self.__name))
+
+
 class Question:
     __rating = int
     __question = str
     __careerEffect = []
     __streamEffect = []
 
-    def __init__(self, question:str) -> None:
+    def __init__(self, question: str) -> None:
         self.__question = question
-    
-    def getData(self, data:str) -> any:
-        if(data == "rating"): return self.__rating
-        elif(data == "question"): return self.__question
-        elif(data == "careerEffect"): return self.__careerEffect
-        elif(data == "streamEffect"): return self.__streamEffect
-        else: raise Exception("data name = {0} not found in Question = {1}".format(data, self.__question))
+
+    def getData(self, data: str) -> any:
+        if (data == "rating"):
+            return self.__rating
+        elif (data == "question"):
+            return self.__question
+        elif (data == "careerEffect"):
+            return self.__careerEffect
+        elif (data == "streamEffect"):
+            return self.__streamEffect
+        else:
+            raise Exception("data name = {0} not found in Question = {1}".format(
+                data, self.__question))
+
 
 class Degree:
     __name = str
@@ -79,26 +119,36 @@ class Degree:
     __difficulty = Difficulty
     __roadMap = []
 
-    def __init__(self, name:str, duration:str, difficulty:Difficulty) -> None:
+    def __init__(self, name: str, duration: str, difficulty: Difficulty) -> None:
         self.__name = name
         self.__duration = duration
         self.__difficulty = difficulty
 
-    def addRoadMap(self, steps:list[str]) -> None:
-        if(len(self.__roadMap) > 0): raise Exception("Road Map is already present in {0}".format(self.__name))
-        else: self.__roadMap = steps
-        
-    def getData(self, data:str) -> any:
-        if(data == "name"): return self.__name
-        elif(data == "duration"): return self.__duration
-        elif(data == "difficulty"): return self.__difficulty
-        elif(data == "roadMap"): return self.__roadMap
-        else: raise Exception("data name = {0} not found in {1} Degree Class".format(data, self.__name))
-        
-    
+    def addRoadMap(self, steps: list[str]) -> None:
+        if (len(self.__roadMap) > 0):
+            raise Exception(
+                "Road Map is already present in {0}".format(self.__name))
+        else:
+            self.__roadMap = steps
+
+    def getData(self, data: str) -> any:
+        if (data == "name"):
+            return self.__name
+        elif (data == "duration"):
+            return self.__duration
+        elif (data == "difficulty"):
+            return self.__difficulty
+        elif (data == "roadMap"):
+            return self.__roadMap
+        else:
+            raise Exception(
+                "data name = {0} not found in {1} Degree Class".format(data, self.__name))
+
+
 class Education(Enum):
     Secondary = 1
     HigherSecondary = 2
+
 
 class Difficulty(Enum):
     Easy = 1
