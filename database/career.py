76e904bfa0c75e23324960be0677a06a0ceeb8e6
gamdevAbhi <115888776+gamdevAbhi@users.noreply.github.com>
2023-04-02 00:10:47 +0530

    Database Update

--- database/career.py
+++ database/career.py
@@ -4,74 +4,110 @@ sys.path.append("src")
 from mentorCB import *
 
 # types of stream
-science = Stream("Science")
+science = Stream("science")
 arts = Stream("Arts")
 commerce = Stream("Commerce")
-general = Stream("General")
 
 # types of degree
+bTech = Degree("Bachelor of Technology", "4 years", Difficulty.Medium)
+mTech = Degree("Master of Technology", "2 Years", Difficulty.Tough)
+bArch = Degree("Bachelor of Architecture", "5 Years", Difficulty.Medium)
+mBBS = Degree("Bachelor of Medicine, Bachelor of Surgery", "5.5 Years", Difficulty.Tough)
+bDS = Degree("Bachelor of Dental Surgery", "5 Years", Difficulty.Medium)
+mS = Degree("Master of Surgery", "3 Years", Difficulty.Tough)
+mD = Degree("Doctor of Medicine", "3 Years", Difficulty.Tough)
+bVSC = Degree("Bachelor of Veterinary Science", "5 Years", Difficulty.Tough)
+bSC = Degree("Bachelor of Sciecne", "3 Years", Difficulty.Medium)
+mSC = Degree("Mster of Science", "2 Years", Difficulty.Medium)
+pHd = Degree("Doctor of Philosophy", "3-6 Years", Difficulty.Tough)
+nDA = Degree("National Defence Academy", "3 Years", Difficulty.Tough)
+bA = Degree("Bachelor of Arts", "3 Years", Difficulty.Medium)
+mA = Degree("Master of Arts", "2 Years", Difficulty.Medium)
+bDes = Degree("Bachelor of Design", "3 Years", Difficulty.Medium)
+bHM = Degree("Bachelor of Hotel Management", "3 Years", Difficulty.Medium)
+bFA = Degree("Bachelor of Fine Arts", "3 Years", Difficulty.Medium)
+lLB = Degree("Bachelor of Legislative Law", "3 Years", Difficulty.Medium)
+bCom = Degree("Bachelor of Commerce", "3 Years", Difficulty.Medium)
+bBA = Degree("Bachelor of Business Administration", "3 Years", Difficulty.Medium)
+mBA = Degree("Master of Business Administration", "3 Years", Difficulty.Medium)
+iCA = Degree("Institute of Chartered Accountants", "3 Years", Difficulty.Tough)
+bEd = Degree("Bachelor of Education", "2 Years", Difficulty.Medium)
+uPSC = Degree("Union Public Service Commision", "Exam Based", Difficulty.Tough)
+sSC = Degree("Staff Selection Commission", "Exam Based", Difficulty.Medium)
+anyD = Degree("Any Degree", "None", Difficulty.Medium)
 
-# types of career 
-computerEngineer = Career("Computer Engineer",Demand.High, "8 LPA")
-civilEngineer = Career("Civil Engineer", Demand.Medium, "5 LPA")
-mechanicalEngineer = Career("Mechanical Engineer", Demand.Low, "4 LPA")
-electricalEngineer = Career("Electrical Engineer", Demand.Medium, "6 LPA")
-chemicalEnginer = Career("Chemical Engineer", Demand.High, "5 LPA")
-autoMobileEngineer = Career("Automobile Engineer", Demand.Low, "4 LPA")
-biotechEngineer = Career("Biotechology Engnieer", Demand.Medium, "4 LPA")
-miningEngineer = Career("Mining Engineer", Demand.High, "5 LPA")
-aerospaceEngineer = Career("Aerospace Engineer", Demand.High, "7 LPA")
+# types of field
+engineering = Field("Engineering", [science])
+medical = Field("Medical", [science])
+sciHonours = Field("Honours", [science])
 
-doctor = Career("Doctor", Demand.High, "8 LPA")
-dentist = Career("Dentist", Demand.High, "6 LPA")
-homeopath = Career("Homeopath Doctor", Demand.Low, "3.5 LPA")
-ayurved = Career("Ayurvedic doctor", Demand.Medium, "5 LPA")
-pharmacist = Career("Pharmacist", Demand.Medium, "2.4 LPA")
-nurse = Career("Nurse", Demand.Medium, "2.4 LPA")
-paramedic = Career("Paramedic", Demand.Medium, "1.6 LPA")
+artsHonours = Field("Honours", [arts])
+fineArts = Field("Fine Arts", [arts])
+law = Field("Law", [arts])
+
+comHonors = Field("Honours", [commerce])
+
+army = Field("Army", [science, arts, commerce])
+goverment = Field("Goverment", [science, arts, commerce])
+general = Field("General", [science, arts, commerce])
+
+# types of career
+comEngineer = Career("Computer Engineer", Demand.High, bTech, 6.4)
+civEngineer = Career("Civil Engineer", Demand.Medium, bTech, 4.5)
+mechEngineer = Career("Mechanical Engineer", Demand.Medium, bTech, 4.2)
+chemEngineer = Career("Chemical Engineer", Demand.Medium, bTech, 4.2)
+elecEngineer = Career("Elecitral Engineer", Demand.Medium, bTech, 5)
+bioEngineer = Career("Biomedical Engineer", Demand.High, bTech, 4)
+petroEngineer = Career("Petroleum Engineer", Demand.High, bTech, 8)
+aeroEngineer = Career("Aerospace Engineer", Demand.High, bTech, 8.2)
+nuclearEngineer = Career("Nuclear Engineer", Demand.High, mTech, 10)
+roboticEngineer = Career("Robotic Engineer", Demand.Medium, bTech, 5)
+rDEngineer = Career("R&D Engineer", Demand.High, mTech, 13)
+genetics = Career("Geneticst", Demand.Medium, mTech, 8)
+architecture = Career("Architecture", Demand.Medium, bArch, 5)
+doctor = Career("Doctor", Demand.High, mBBS, 8)
+dentist = Career("Dentist", Demand.Medium, bDS, 5)
+surgeon = Career("Surgeon", Demand.High, mS, 12)
+psychiatrist = Career("Psychiatrist", Demand.High, mD, 10)
+veterinarian = Career("Veterinarian", Demand.Medium, bVSC, 6.2)
+bioTech = Career("Biotechnologist", Demand.Medium, bSC, 5)
+microBio = Career("Microbiologist", Demand.Medium, bSC, 3.4)
+forensic = Career("Forensic", Demand.Medium, bSC, 5)
+bioChem = Career("Biochemist", Demand.Medium, bSC, 3.8)
+pharma = Career("Pharamicst", Demand.High, bSC, 4)
+researchSci = Career("Research Scientist", Demand.High, pHd, 8)
+foodTech = Career("Food Technologist", Demand.Medium, bSC, 4)
+fightPilot = Career("Fighter Pilot", Demand.High, nDA, 17)
+psychologist = Career("Psychologist", Demand.Medium, bSC, 5)
+mHCouselor = Career("Mental Health Counselor", Demand.Medium, mSC, 6)
+meteorologists = Career("Meteorologists", Demand.Medium, mSC, 4)
+navOff = Career("Navy Officer", Demand.High, nDA, 7)
+nurse = Career("Nurse", Demand.Medium, bSC, 4)
+agriIns = Career("Agricultural Inspector", Demand.Medium, bSC, 4.5)
+marBio = Career("Marine Biologist", Demand.Medium, mSC, 7)
+oTT = Career("Operation Theatre Technologists", Demand.Medium, bSC, 3.6)
+astronomer = Career("Astronomer", Demand.High, pHd, 10)
+athmoRe = Career("Atmospheric Researchers", Demand.Medium, mSC, 6)
+zoologist = Career("Zoologist", Demand.Medium, bSC, 4)
+wildBio = Career("Wildlife Biologist", Demand.Medium, mSC, 6)
+epidlogist = Career("Epidemiologist", Demand.Medium, mSC, 6)
 
 # types of questions
 
+# database of degrees
 
-
-# database Roadmap
-
-
-#database career
-
-
-# database stream
-science.addCareer(computerEngineer)
-science.addCareer(civilEngineer)
-science.addCareer(mechanicalEngineer)
-science.addCareer(electricalEngineer)
-science.addCareer(chemicalEnginer)
-science.addCareer(autoMobileEngineer)
-science.addCareer(biotechEngineer)
-science.addCareer(miningEngineer)
-science.addCareer(aerospaceEngineer)
-
-science.addCareer(doctor)
-science.addCareer(dentist)
-science.addCareer(homeopath)
-science.addCareer(ayurved)
-science.addCareer(pharmacist)
-science.addCareer(paramedic)
-
+# database of field
+engineering.defineCareers([comEngineer, bioEngineer, civEngineer, aeroEngineer, chemEngineer, elecEngineer, mechEngineer,
+petroEngineer, nuclearEngineer, roboticEngineer, rDEngineer, genetics, architecture])
+medical.defineCareers([doctor, dentist, surgeon, psychiatrist, veterinarian, pharma, nurse, oTT])
+sciHonours.defineCareers([bioTech, microBio, forensic, bioChem, researchSci, foodTech, psychologist, mHCouselor, 
+meteorologists, agriIns, marBio, astronomer, athmoRe, zoologist, wildBio, epidlogist])
 
 # test
-for career in science.getData("careers"):
-    print("Career =", career.getData("name"))
-    print("Demand =", career.getData("demand").name)
-    print("Annual Salary =", career.getData("annualSalary"))
-    print("Degrees->")
-    print(".........................")
 
-    for degree in career.getData("degrees"):
-        print("...")
-        print(degree.getData("name"))
-        print("Duration =", degree.getData("duration"))
-        print("Difficulty =", degree.getData("difficulty").name)
-        print("Road Map =", degree.getData("roadMap"))
-
-    print(".........................")
+for field in fields:
+    print(field.getData("name"), "strem ->", field.getData("streams"))
+    print("---------------------")
+    for career in field.getData("careers"):
+        print(career.getData("name"))
+    print("---------------------")
--- src/mentorCB.py
+++ src/mentorCB.py
@@ -1,62 +1,89 @@
 from enum import Enum
 
-class Career: pass
 class Stream: pass
+class Field: pass
+class Career: pass
 class Question: pass
 class Degree: pass
 
-class Education: pass
 class Difficulty: pass
 class Demand: pass
+
+fields = []
 
 class Stream:
     __name = str
     __likeable = float
-    __careers = None
+    __questions = None
 
     def __init__(self, name:str) -> None:
         self.__name = name
         self.__likeable = 0.0
-        self.__careers = []
-    
-    def addCareer(self, career:Career) -> None:
-        self.__careers.append(career)
-        career.changeStream(self)
+        self.__questions = []
+
+    def addQuestion(self, question:Question) -> None:
+        if(self.__questions.__contains__(question) == False): self.__questions.append(question)
+        else: raise Exception("question {0} already exist in the {1}".format(question.getData("question"), self.__name))
     
     def getData(self, data:str) -> any:
         if(data == "name"): return self.__name
         elif(data == "likeable"): return self.__likeable
-        elif(data == "careers"): return self.__careers
+        elif(data == "questions"): return self.__questions
         else: raise Exception("data name = {0} not found in {1} Stream Class".format(data, self.__name))
+
+class Field:
+    __name = str
+    __likeable = float
+    __streams = None
+    __careers = None
+    __questions = None
+
+    def __init__(self, name:str, streams:list) -> None:
+        global fields
+        self.__name = name
+        self.__likeable = 0.0
+        self.__streams = streams
+        self.__careers = []
+        self.__questions = []
+        
+        fields.append(self)
+    
+    def defineCareers(self, careers:list[Career]) -> None:
+        if(len(self.__careers) == 0): self.__careers = careers
+        else: raise Exception("careers are already exist in the {1}".format(self.__name))
+    
+    def defineQuestions(self, questions:list[Question]) -> None:
+        if(len(self.__questions) == 0): self.__questions = questions
+        else: raise Exception("questions are already exist in the {1}".format(self.__name))
+
+    def getData(self, data:str):
+        if(data == "name"): return self.__name
+        elif(data == "likeable"): return self.__likeable
+        elif(data == "streams"): return self.__streams
+        elif(data == "careers"): return self.__careers
+        elif(data == "questions"): return self.__questions
+        else: raise Exception("data {0} is not found in {1}".format(data, self.__name))
+
+
 
 class Career:
     __name = str
-    __stream = None
-    __degrees = None
+    __degree = Degree
     __likable = float
     __demand = Demand
-    __annualSalary = str
+    __annualSalary = float
 
-    def __init__(self, name:str, demand:Demand, annualSalary:str) -> None:
+    def __init__(self, name:str, demand:Demand, degree:Degree, annualSalary:float) -> None:
         self.__name = name
         self.__likable = 0.0
-        self.__degrees = []
         self.__demand = demand
         self.__annualSalary = annualSalary
-
-    def addDegree(self, degree:Degree):
-        self.__degrees.append(degree)
-    
-    def changeStream(self, stream:Stream):
-        if(self.__stream != None): raise Exception("{0} already in {1} so it can't enter {2}".format(self.__name, 
-        self.__stream.getData("name"), stream.getData("name")))
-        else: self.__stream = stream
+        self.__degree = degree
     
     def getData(self, data:str) -> any:
         if(data == "name"): return self.__name
-        elif(data == "stream"): return self.__stream
-        elif(data == "degrees"): return self.__degrees
         elif(data == "likable"): return self.__likable
+        elif(data == "degree"): return self.__degree
         elif(data == "demand"): return self.__demand
         elif(data == "annualSalary"): return self.__annualSalary
         else: raise Exception("data name = {0} not found in {1} Career Class".format(data, self.__name))
@@ -64,19 +91,16 @@ class Career:
 class Question:
     __rating = int
     __question = str
-    __careerEffect = None
-    __streamEffect = None
+    __effects = None
 
     def __init__(self, question:str) -> None:
         self.__question = question
-        self.__careerEffect = []
-        self.__streamEffect = []
+        self.__effects = []
     
     def getData(self, data:str) -> any:
         if(data == "rating"): return self.__rating
         elif(data == "question"): return self.__question
-        elif(data == "careerEffect"): return self.__careerEffect
-        elif(data == "streamEffect"): return self.__streamEffect
+        elif(data == "effects"): return self.__effects
         else: raise Exception("data name = {0} not found in Question = {1}".format(data, self.__question))
 
 class Degree:
@@ -91,7 +115,7 @@ class Degree:
         self.__difficulty = difficulty
         self.__roadMap = []
 
-    def addRoadMap(self, steps:list[str]) -> None:
+    def defineRoadMap(self, steps:list[str]) -> None:
         if(len(self.__roadMap) > 0): raise Exception("Road Map is already present in {0}".format(self.__name))
         else: self.__roadMap = steps
         
